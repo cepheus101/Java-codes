28c4c5f3f4c4967f126085490448e333f13e17f7
Cepheus <117722348+Cepheus2005@users.noreply.github.com>
2022-12-02 00:00:12 +0530

    Add files via upload

---
+++ editor.java
@@ -0,0 +1,93 @@
+// Java Program to create a text editor using java
+import javax.swing.*;
+import java.awt.event.*;
+import javax.swing.plaf.metal.*;
+class editor extends JFrame implements ActionListener 
+{
+	// Text component
+	JTextArea t;
+
+	// Frame
+	JFrame f;
+
+	// Constructor
+	editor()
+	{
+		// Create a frame
+		f = new JFrame("editor");
+
+		try {
+			// Set metal look and feel
+			UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+
+			// Set theme to ocean
+			MetalLookAndFeel.setCurrentTheme(new OceanTheme());
+		}
+		catch (Exception e) {
+		}
+
+		// Text component
+		t = new JTextArea();
+
+		// Create a menubar
+		JMenuBar mb = new JMenuBar();
+
+		// Create amenu for menu
+		JMenu m = new JMenu("Edit");
+
+		// Create menu items
+		JMenuItem mi1 = new JMenuItem("cut");
+		JMenuItem mi2 = new JMenuItem("copy");
+		JMenuItem mi3 = new JMenuItem("paste");
+
+		// Add action listener
+		mi1.addActionListener(this);
+		mi2.addActionListener(this);
+		mi3.addActionListener(this);
+
+		m.add(mi1);
+		m.add(mi2);
+		m.add(mi3);
+
+		JMenuItem mc = new JMenuItem("close");
+
+		mc.addActionListener(this);
+
+		mb.add(m);
+		mb.add(mc);
+
+		f.setJMenuBar(mb);
+		f.add(t);
+		f.setSize(500, 500);
+		f.show();
+	}
+
+	// If a button is pressed
+	public void actionPerformed(ActionEvent e)
+	{
+		String s = e.getActionCommand();
+
+		if (s.equals("cut")) 
+		{
+			t.cut();
+		}
+		else if (s.equals("copy")) 
+		{
+			t.copy();
+		}
+		else if (s.equals("paste")) 
+		{
+			t.paste();
+		}
+		else if (s.equals("close")) 
+		{
+			f.setVisible(false);
+		}
+	}
+
+	// Main class
+	public static void main(String args[])
+	{
+		editor e = new editor();
+	}
+}
