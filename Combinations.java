275803a763e9f98421119a1676c69cc273561442
Cepheus <cepheus2005@gmail.com>
2023-08-02 15:55:49 +0000

    Update

--- Combinations.java
+++ Combinations.java
@@ -1,41 +1,29 @@
 import java.util.Scanner;
 
-public class Combinations 
-{
-    public static void main(String[] args) 
-    {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int N = sc.nextInt();
+public class Combinations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("N = ");
+        int N = scanner.nextInt();
+        System.out.println("Output:");
         int count = 0;
-        for (int i = 0; i < Math.pow(2, N); i++) 
-        {
+        int max = (int) Math.pow(2, N);
+        for (int i = 0; i < max; i++) {
             int sum = 0;
-            for (int j = 0; j < N; j++) 
-            {
-                if ((i & (int)Math.pow(2, j)) != 0) 
-                {
+            String combination = "";
+            int temp = i;
+            for (int j = 0; j < N; j++) {
+                if (temp % 2 == 1) {
                     sum += j + 1;
-                } 
-                else 
-                {
+                    combination += " " + (j + 1);
+                } else {
                     sum -= j + 1;
+                    combination += " -" + (j + 1);
                 }
+                temp /= 2;
             }
-            if (sum == 0) 
-            {
-                for (int j = 0; j < N; j++) 
-                {
-                    if ((i & (int)Math.pow(2, j)) != 0) 
-                    {
-                        System.out.print((j + 1) + " ");
-                    } 
-                    else 
-                    {
-                        System.out.print(- (j + 1) + " ");
-                    }
-                }
-                System.out.println();
+            if (sum == 0) {
+                System.out.println(combination.trim());
                 count++;
             }
         }
